b3f2a6bd886e22aa4ec788116c177c4bfad4ba44
denis_adidas <veb-kamera2013@ya.ru>
2022-10-10 21:37:17 +0300

    Create main.cpp

---
+++ lab18/main.cpp
@@ -0,0 +1,18 @@
+#include "lab18_class_impl.h"
+
+
+int main() {
+
+      multiset<int> a(10);
+      a.insert(10);
+      a.insert(25);
+      a.insert(6);
+      a.insert(2);
+
+      std::cout << a;
+
+
+
+
+    return 0;
+}
