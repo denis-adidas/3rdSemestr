efd71910904a6f9868d95987ffc1cab90648c299
denis_adidas <veb-kamera2013@ya.ru>
2022-10-13 18:38:14 +0300

    Update lab18_class_impl.h

--- lab18/lab18_class_impl.h
+++ lab18/lab18_class_impl.h
@@ -1,26 +1,35 @@
 //
 // Created by Денис Адидас on 9/27/22.
+// посчитатать кол-во уникальных слов с помощью мултисета
 //
 #include "lab18_class.h"
 
 template <class T>
 multiset<T>::multiset(T i) {
-    size++;
+    size=1;
     data.template emplace(i, 1);
 }
+
+template <class T>
+multiset<T>::multiset() {
+    size=0;
+}
+
 template <class T>
 multiset<T>::~multiset() {
     data.clear();
 }
 
 template <class T>
-void multiset<T>::insert(int val) {
+void multiset<T>::insert(T val) {
     if (data.find(val) != data.end()) {
         data[val]++;
         size++;
     }
-    else
+    else {
         data.template emplace(val, 1);
+        size++;
+    }
 }
 
 template <class T>
@@ -35,6 +44,8 @@ void multiset<T>::erase(int val) {
         throw std::invalid_argument("No value at such index");
     }
 }
+
+
 
 template <class T>
 int multiset<T>::find(int index) {
@@ -55,7 +66,9 @@ std::ostream& operator<<(std::ostream& os, const multiset<T>& a){
     return os;
 };
 
-
-
+template <typename T>
+int multiset<T>::get_size() {
+    return data.size();
+}
 
 
