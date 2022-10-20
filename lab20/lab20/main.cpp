646d9d24cbb39484f0905c6865ea37d20fbdc16e
denis_adidas <veb-kamera2013@ya.ru>
2022-10-20 17:44:02 +0300

    1

---
+++ lab20/lab20/main.cpp
@@ -0,0 +1,14 @@
+//
+//  main.cpp
+//  lab20
+//
+//  Created by Денис Адидас on 10/20/22.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World! and lab20\n";
+    return 0;
+}
