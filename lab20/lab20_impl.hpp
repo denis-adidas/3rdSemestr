df9e4d5ba3a7b7f09202674bc2be00822a644faa
denis_adidas <veb-kamera2013@ya.ru>
2022-10-31 15:38:43 +0300

    Create lab20_impl.hpp

---
+++ lab20/lab20_impl.hpp
@@ -0,0 +1,22 @@
+//
+//  lab20_impl.hpp
+//  lab20
+//
+//  Created by Денис Адидас on 10/21/22.
+//
+
+#ifndef lab20_impl_hpp
+#define lab20_impl_hpp
+
+#include <iostream>
+#include "vector"
+#include "string"
+#include "fstream"
+#include "stdlib.h"
+
+using namespace std;
+
+void dfs(int start, vector<bool> & visited, const vector <vector <int> > g);
+
+
+#endif /* lab20_impl_hpp */
