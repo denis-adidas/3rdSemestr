004d9d451e8fb2f7bf10ebba55e91839cfe5fc44
denis_adidas <veb-kamera2013@ya.ru>
2022-10-31 15:38:45 +0300

    Create main.cpp

---
+++ lab20/main.cpp
@@ -0,0 +1,63 @@
+//
+//  main.cpp
+//  lab20
+//
+//  Created by Денис Адидас on 10/21/22.
+//
+
+#include "lab20_impl.hpp"
+
+using namespace std;
+
+
+int main(int argc, const char * argv[]) {
+    
+    int n, m;
+    
+    string s;
+    ifstream file("/Users/denis_adidas/XCode project/lab20/lab20/graph_input.txt");
+    ofstream fout("/Users/denis_adidas/XCode project/lab20/lab20/graph_output.txt");
+    
+    getline(file,s);
+    char* cstr = new char[s.length()+1];
+    strcpy(cstr, s.c_str());
+    
+    char* token = strtok(cstr, " \n");
+    n = atoi(token);
+    token = strtok(NULL, " \n");
+    m = atoi(token);
+    
+    
+    
+    
+    vector<vector<int>> graph(n);
+    int u, v;
+
+    for (int i = 0; i < m; i++) {
+        getline(file, s);
+        strcpy(cstr, s.c_str());
+        token = strtok(cstr, " \n");
+        u = atoi(token);
+        token = strtok(NULL, " \n");
+        v = atoi(token);
+        u--; v--;
+        graph[u].push_back(v);
+    }
+//        cout << graph[3][0] << "!!" << endl;
+
+
+    
+    int start = 0;
+    while (start < 5) {
+        vector <bool> visited(n);
+        dfs(start, visited, graph);
+        fout << "Result for " << start + 1 << ": ";
+        for (int i = start; i < visited.size(); i++)
+            if (visited[i] == true) fout << i + 1 << " ";
+        fout << endl;
+        start++;
+    }
+    file.close();
+    fout.close();
+    return 0;
+}
