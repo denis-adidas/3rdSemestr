ef930261c5646324964a004957205868d3b1ada9
denis_adidas <veb-kamera2013@ya.ru>
2022-11-04 14:18:39 +0300

    Create main.cpp

---
+++ lab20/lab20_ex/main.cpp
@@ -0,0 +1,50 @@
+#include "lab20_impl.h"
+
+using namespace std;
+
+
+int main(int argc, const char * argv[]) {
+
+    int n, m;
+
+    string s;
+    ifstream file("/Users/denis_adidas/CLionProjects/lab20/graph_input.txt");
+    ofstream fout("/Users/denis_adidas/CLionProjects/lab20/output.dot");
+
+    getline(file,s);
+    char* cstr = new char[s.length()+1];
+    strcpy(cstr, s.c_str());
+
+    char* token = strtok(cstr, " \n");
+    n = atoi(token);
+    token = strtok(NULL, " \n");
+    m = atoi(token);
+
+    vector<vector<int>> graph(n);
+    int u, v;
+
+    //обрабатваем граф
+    for (int i = 0; i < m; i++) {
+        getline(file, s);
+        strcpy(cstr, s.c_str());
+        token = strtok(cstr, " \n");
+        u = atoi(token);
+        token = strtok(NULL, " \n");
+        v = atoi(token);
+        u--; v--;
+        graph[u].push_back(v);
+    }
+
+    vector<string> dfs_nodes(n);
+    fill_nodes(dfs_nodes, graph, n);
+
+
+    show_graph(fout, dfs_nodes, graph);
+
+    file.close();
+    fout.close();
+
+
+    return 0;
+}
+
