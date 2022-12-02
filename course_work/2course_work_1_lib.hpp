7a781be71da5494856cf4c3765464287f0df6409
denis_adidas <veb-kamera2013@ya.ru>
2022-12-02 11:09:24 +0300

    Update 2course_work_1_lib.hpp

--- course_work/2course_work_1_lib.hpp
+++ course_work/2course_work_1_lib.hpp
@@ -9,9 +9,13 @@
 #define _course_work_1_lib_hpp
 
 #include "iostream"
-
+#include <chrono>
+#include "cstdlib"
 #include "fstream"
 #include "vector"
+#include <cmath>
+#include <sstream>
+#include <algorithm>
 
 using namespace std;
 
@@ -20,67 +24,24 @@ protected:
     vector<vector<int>> g;
     vector<int> way, min_way;
     vector<int> visited;
-    vector<int> fullGraph;
-    int size, min_distance, distance;
-    int nodes;
+    vector<int> final_path;
+    int distance, nodes;
+    int final_result;
     
 public:
-    graph(string path) {
-        size = 0;
-        string s;
-        ifstream file(path);
-        getline(file, s);
-        char* cstr = new char[s.length()+1];
-        strcpy(cstr, s.c_str());
-            
-        
-        char* token = strtok(cstr, " \n");
-        nodes = atoi(token);
-            
-        g.resize(nodes);
-        
-        int v;
-        for (int i = 0; i < nodes; i++) {
-            getline(file, s);
-            strcpy(cstr, s.c_str());
-            token = strtok(cstr, " \n");
-            while (token != NULL) {
-                v = atoi(token);
-                g[i].push_back(v);
-                token = strtok(NULL, " \n");
-            }
-        }
-        file.close();
-        visited.resize(nodes);
-        min_distance = 100;
-        distance = 0;
-    }
-    int sum() {
-        int sum = 0;
-        for (auto i : visited)
-                sum += i;
-        return sum;
-    }
-    void dfs(int v, int v_start);
+    graph(string path);
+    int sum();
     void solve(int source, int current);
-    void ShowGraph() {
-        for (auto i : way) {
+    void copyToFinal(std::vector<int>& way);
+    int firstMinInRow(int vertex_index);
+    int secondMinInRow(int i);
+    void TSPRec(int curr_bound, int current_weight, int level, std::vector<int> way, std::vector<int> vis);
+    void TSP();
+    void show_res() {
+        for (auto i : final_path) {
             cout << i << " ";
         }
     }
-    int min_in_str(vector<int>& a, vector<int>& used) {
-        int min = 100;
-        int index = -1;
-        for (auto i : a) {
-            if (a[i] < min && !used[i]){
-                min = a[i];
-                index = i;
-            }
-        }
-        return index;
-    }
-    int path(int current);
-    void minimal(int current);
-    
 };
+
 #endif /* _course_work_1_lib_hpp */
