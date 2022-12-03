6ed52bf3222a9ee5c46da3e459ab15361d7dd9da
denis_adidas <veb-kamera2013@ya.ru>
2022-12-03 22:29:11 +0300

    Update main.cpp

--- course_work/main.cpp
+++ course_work/main.cpp
@@ -13,14 +13,18 @@ using namespace std;
 int main(int argc, const char * argv[]) {
     string path = "/Users/denis_adidas/XCode project/2course_work_1/2course_work_1/matrix.txt";
 
-    int n;
+//    int n;
 //    cout << "Enter a nodes: " << endl;
 //    cin >> n;
     graph a(path);
+
+      a.greedy(0);
 //    a.solve(0, 0);
-//    cout << "Nodes: " << n;
-    a.TSP();
-    a.show_res();
+//    a.show_res();
+    
+//    a.TSP();
+//    a.checkResult();
+//    a.show_res();
     
     
 //    a.dfs(0, 0);
