8227cf972d7f1d27996c6b4e2a8f82f394d511f5
denis_adidas <veb-kamera2013@ya.ru>
2022-12-03 22:29:18 +0300

    Update 2course_work_1_lib.hpp

--- course_work/2course_work_1_lib.hpp
+++ course_work/2course_work_1_lib.hpp
@@ -13,7 +13,6 @@
 #include "cstdlib"
 #include "fstream"
 #include "vector"
-#include <cmath>
 #include <sstream>
 #include <algorithm>
 
@@ -22,7 +21,7 @@ using namespace std;
 class graph {
 protected:
     vector<vector<int>> g;
-    vector<int> way, min_way;
+    vector<int> way;
     vector<int> visited;
     vector<int> final_path;
     int distance, nodes;
@@ -33,14 +32,37 @@ public:
     int sum();
     void solve(int source, int current);
     void copyToFinal(std::vector<int>& way);
-    int firstMinInRow(int vertex_index);
-    int secondMinInRow(int i);
+    int FirstMinInRow(int vertex_index);
+    int SecondMinInRow(int i);
+    void greedy(int source);   
     void TSPRec(int curr_bound, int current_weight, int level, std::vector<int> way, std::vector<int> vis);
     void TSP();
-    void show_res() {
-        for (auto i : final_path) {
-            cout << i << " ";
+    
+    void print_graph() {
+        ofstream fout("/Users/denis_adidas/XCode project/2course_work_1/2course_work_1/output.dot");
+        fout << "digraph test { ";
+        for (auto i : g)
+            for (auto j : i) {
+                fout << "->" << j + 1;
+            }
+        fout << "}";
+    }
+    
+    void checkResult() {
+        int flag = 0;
+        for (int i = 0; i < final_path.size(); i++) {
+            if (final_path[i] > 0) flag++;
         }
+        if (flag != nodes - 1) {
+            cout << "Some nodes aren't connected! \nPlease, check source file!" << endl;
+        }
+    }
+    void show_res() {
+        cout << "Way: ";
+        for (int i = final_path.size() - nodes - 1; i < final_path.size(); i++) {
+            cout << final_path[i] + 1 << " ";
+        }
+        cout << endl << "Distance: " << final_result << endl;
     }
 };
 
