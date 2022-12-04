735e88a0a9f326857041a65de1c45c5c760dd814
denis_adidas <veb-kamera2013@ya.ru>
2022-12-04 13:32:33 +0300

    Update 2course_work_1_lib.cpp

--- course_work/2course_work_1_lib.cpp
+++ course_work/2course_work_1_lib.cpp
@@ -15,13 +15,13 @@ graph::graph(string path) {
     getline(file, s);
     char* cstr = new char[s.length()+1];
     strcpy(cstr, s.c_str());
-        
-    
+
+
     char* token = strtok(cstr, " \n");
     nodes = atoi(token);
-        
+
     g.resize(nodes);
-    
+
     int v;
     for (int i = 0; i < nodes; i++) {
         getline(file, s);
@@ -45,54 +45,77 @@ int graph::sum() {
 }
 
 void graph::greedy(int source) {
-    
+
     visited.resize(nodes);
     final_path.resize(nodes);
     final_result = INT_MAX;
     distance = 0;
-    
+    int time = clock();
+
     solve(source, 0);
-    checkResult();
-    
-    cout << source + 1 << " ";
+
+    time = clock() - time;
+    final_path.insert(final_path.begin(), source);
+
+    int flag = 0;
+    for (int i = 0; i < final_path.size(); i++) {
+        if (final_path[i] > 0) flag++;
+    }
+    if (flag != nodes - 1) {
+        cout << "Some nodes aren't connected! \nPlease, check source file!" << endl;
+    }
+
+    cout << "Greedy's solve of TSP: " << endl;
     for (auto i : final_path) {
         cout << i + 1 << " ";
     }
     cout << endl;
     cout << "Distance: " << final_result << endl;
+    cout << "Time: " << time << endl;
+
+    ofstream fout("/Users/denis_adidas/CLionProjects/2courses_work/output_greedy.dot");
+    fout << "digraph test { ";
+    fout << source + 1;
+    for (int i = 1; i < final_path.size(); i++) {
+        fout << "->" << final_path[i] + 1;
+        fout << endl;
+    }
+    fout << "}";
+
     return;
 }
 
 void graph::solve(int source, int current) {
-    
+
+
     if ((current == 0) && (sum() == nodes)) {
         if (distance < final_result){
             final_result = distance;
             for(int i = 0; i < nodes; i++)
                 final_path[i] = way[i];
-            
-        }
-    }
-        if (visited[current] == 1) {
             return;
         }
-        
-        visited[current] = 1;
-        vector<int> used(nodes);
-        used[current] = 1;
-        
-        
-        for(int i = 0; i < nodes; i++){
-            if(!used[i] && g[current][i]){
-                distance += g[current][i];
-                way.push_back(i);
-                solve(source, i);
-                distance -= g[current][i];
-                way.pop_back();
-                used[i] = 0;
-            }
-        }
-        visited[current] = 0;
+    }
+    if (visited[current] == 1) {
         return;
     }
+
+    visited[current] = 1;
+    vector<int> used(nodes);
+    used[current] = 1;
+
+
+    for(int i = 0; i < nodes; i++){
+        if(!used[i] && g[current][i]){
+            distance += g[current][i];
+            way.push_back(i);
+            solve(source, i);
+            distance -= g[current][i];
+            way.pop_back();
+            used[i] = 0;
+        }
+    }
+//    visited[current] = 0;
+    return;
+}
 
